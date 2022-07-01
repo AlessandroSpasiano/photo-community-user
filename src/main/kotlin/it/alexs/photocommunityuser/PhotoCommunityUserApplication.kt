06afd696a144606e1f319a4382449827c6ac7b02
Alessandro Spasiano <alessandrospasiano@snps.it>
2022-07-01 09:14:32 +0200

    initial commit

---
+++ src/main/kotlin/it/alexs/photocommunityuser/PhotoCommunityUserApplication.kt
@@ -0,0 +1,11 @@
+package it.alexs.photocommunityuser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhotoCommunityUserApplication
+
+fun main(args: Array<String>) {
+    runApplication<PhotoCommunityUserApplication>(*args)
+}
---
+++ src/test/kotlin/it/alexs/photocommunityuser/PhotoCommunityUserApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.alexs.photocommunityuser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhotoCommunityUserApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
